b738378430ec8b7d085272ec0c1d6c69b0be16b5
Daryan Zycki <da.zycki@gmail.com>
2022-03-01 05:33:53 +0000

    Complete Testing Cats as a Service module from compass.

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,20 @@
 const request = require('request');
 
-const args = process.argv.slice(2); //take in argument from command line
-const BREED = String(args[0]);
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      callback(error);
+    }
+    
+    const data = JSON.parse(body);
+    
+    if (data.length === 0) {
+      callback('Cat breed not found, please try again!');
+    } else if (data) {
+      callback(error, data[0]['description']);
+      console.log(error);
+    }
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${BREED}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  }
-  
-  const data = JSON.parse(body);
-  
-  if (data.length === 0) {
-    console.log('Cat breed not found, please try again!');
-  } else if (data) {
-    console.log(data[0]['description']);
-  }
-});
+module.exports = { fetchBreedDescription };
